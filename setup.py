cd360ff929455f472c1cc95ea15a1609b146f036
Brandon R. Stoner <github@monokro.me>
2010-08-14 17:54:36 -0600

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='payment',
+    description='A django application for keeping your fans up to date... or whatever else you may do.',
+    author='Brandon R. Stoner',
+    author_email='monokrome@monokro.me',
+    url='http://monokro.me',
+    download_url='http://github.com/monokrome/python-payment/',
+    packages=['payment'],
+)
+
