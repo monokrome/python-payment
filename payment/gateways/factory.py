fcb5660e0409ede7eebaa1ae4c1d230af332381b
Brandon R. Stoner <monokrome@monokro.me>
2010-09-25 16:38:47 -0600

    Ammending.

--- payment/gateways/factory.py
+++ payment/gateways/factory.py
@@ -1,20 +1,24 @@
 from exceptions import ImportError, NameError
 import re
 
-gateway_name_conversion_expression = '/^\s+|^\d+|[^\w\d]|\s$/'
+gateway_name_conversion_expression = '/^\d+|^\d+|[^\w\d]|\s$/'
 
 class GatewayFactory(object):
     """ Used to recieve gateway objects based with directly importing. """
 
-    @staticmethod
-    def get_by_name(gateway_name):
+    def get_by_name(self, gateway_name):
         """ Gets a gateway object based on the gateway's real name. """
         gateway_classname = re.sub(gateway_name_conversion_expression, '',
             gateway_name.title()
         )
 
-        # TODO: Figure out how to import this relative
-        gateway_modulename = 'payment.gateways.%s' % gateway_classname.lower()
+        return self.get_by_classname(gateway_classname)
+
+    def get_by_classname(self, gateway_classname):
+        """ Returns a gateway class based on the provided class name """
+        module_name = self.get_module_name(gateway_classname)
+
+        gateway_modulename = 'payment.gateways.%s' % self.get_module_name(gateway_classname)
 
         gateway_module = __import__(
                    gateway_modulename,
@@ -30,4 +34,8 @@ class GatewayFactory(object):
             raise NameError('Module %s for "%s" doesn''t define "%s"' %
                        (gateway_modulename, gateway_name, gateway_classname)
                            )
+
+    def get_module_name(self, class_name):
+        """ Takes a classname and returns a module name for that class name """
+        return class_name.lower()
 
