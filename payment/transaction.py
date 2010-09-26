963fc932f408aef0db09a8922a38b9e65c3b3df5
Brandon R. Stoner <monokrome@monokro.me>
2010-09-26 15:43:59 -0600

    Modified the processing methods.

--- payment/transaction.py
+++ payment/transaction.py
@@ -1,23 +1,43 @@
+from errors import NoGatewayError
+
+no_gateway_error_text = 'You must provide at least one gateway for processing'
+
 class Transaction(object):
     gateways = []
     identifier = None
     success = None
 
-    def charge(self):
+    def process(self, method=None):
+        """ Performs an arbitrary type of processing through the gateway(s). """
+        if len(self.gateways) < 1:
+            raise NoGatewayError(no_gateway_error_text)
+
+        self.success = False
+
+        for gateway in self.gateways:
+            if hasattr(gateway, method):
+                method_reference = getattr(gateway, method)
+                gateway_transaction = method_reference(transaction=self)
+
+                if gateway.success == True:
+                    self.success = True
+                    return gateway_transaction
+
+    def charge(self, gateway=None):
         """ Starts the process of transfering the funds from our payment
             method through our gateway. """
-        pass
+        return self.process(gateway=gateway, method='charge')
 
-    def authorize(self):
+    def authorize(self, gateway=None):
         """ An authorization is made in order to check whether the funds
             are available without actually charging the card. """
-        pass
+        return self.process(gateway=gateway, method='authorize')
 
     def credit(self):
         """ Credits an amount of money back to the method of payment. """
-        pass
+        return self.process(gateway=gateway, method='credit')
 
     def void(self):
         """ Cancels a transactions.. """
-        pass
+        return self.process(gateway=gateway, method='void')
 
